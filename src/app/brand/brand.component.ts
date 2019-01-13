20a4173156d894dc2f2a6fa3530fbb1f7066d27f
Victor Villavicencio <victor.villavicencio.10@gmail.com>
2019-01-13 17:54:32 -0400

    nav style and drawer style

---
+++ src/app/brand/brand.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-brand',
+  templateUrl: './brand.component.html',
+  styleUrls: ['./brand.component.sass']
+})
+export class BrandComponent {
+  isNav: boolean;
+  @HostListener("window:scroll", ['$event'])
+  doSomethingOnWindowsScroll($event:Event){
+    let scrollOffset = $event.srcElement.children[0].scrollTop;
+    if (scrollOffset < 5) {
+      this.isNav = false;
+    }
+    else {
+      this.isNav = true;
+    }
+  }
+  constructor() { 
+  }
+
+}
